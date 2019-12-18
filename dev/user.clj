33611bdbe917e985c07bbba3cb7b5cfb44441297
Caio Oliveira <caioaao@gmail.com>
2019-12-18 18:23:35 -0500

    init

---
+++ dev/user.clj
@@ -0,0 +1,10 @@
+(ns user
+  (:require [hashp.core]
+            [clojure.main]
+            [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+            [orchestra.spec.test :as st]))
+(set! *warn-on-reflection* true)
+
+(defn -main [& args]
+  (st/instrument)
+  (apply clojure.main/main args))
---
+++ src/liftbridge/client.clj
@@ -0,0 +1,2 @@
+(ns liftbridge.client)
+
