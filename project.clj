29987b3af9752ab9593d7d8bc3f66f8a130e99ff
Caio Oliveira <caioaao@gmail.com>
2019-12-19 09:38:29 -0500

    reorg

--- project.clj
+++ project.clj
@@ -7,13 +7,16 @@
              "BUILD_NUMBER" ~(System/getenv "BUILD_NUMBER")}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :source-paths ["src/clojure"]
-  :java-source-paths ["src/java"]
+  :source-paths ["src/main/clojure"]
+  :java-source-paths ["src/main/java"
+                      "src/auto/java"]
+  :resource-paths ["resources/main"]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.google.protobuf/protobuf-java "3.11.0"]
                  [io.grpc/grpc-protobuf "1.26.0"]
                  [io.grpc/grpc-stub "1.26.0"]]
-  :profiles {:dev {:source-paths ["src/clojure" "dev"]
+  :profiles {:dev {:source-paths ["src/main/clojure" "src/dev/clojure"]
+                   :resource-paths ["resources/dev"]
                    :repl-options {:init-ns user}
                    :dependencies [[hashp "0.1.0"]
                                   [orchestra "2018.12.06-2"]
