91e650b90e3948e8bfa98aee86e0faf4a21b1f10
Caio Oliveira <caioaao@gmail.com>
2019-12-19 13:55:59 -0500

    fix timestamp and async code

--- src/main/clojure/liftbridge/client.clj
+++ src/main/clojure/liftbridge/client.clj
@@ -7,7 +7,8 @@
            [io.grpc.stub StreamObserver]
            [com.google.protobuf ByteString])
   (:require [clojure.java.io :as io]
-            [clojure.core.async :as async]))
+            [clojure.core.async :as async]
+            [liftbridge.message]))
 
 (defprotocol IClient
   (create-stream
@@ -53,17 +54,15 @@
        - `:liftbridge.start-at/offset` Uses `start-offset` to set start
          position.
 
-       - `:liftbridge.start-at/time` Uses `start-time` option to set start
-         position.
-
-       - `:liftbridge.start-at/time-delta` Uses `start-time-delta` option to set
-         start position.
+       - `:liftbridge.start-at/timestamp` Uses `start-timestamp-nanos` option to
+         set start position.
 
     - `start-offset` Sets the subscription start position to the first
       message with an offset greater than or equal to the given offset.
 
-    - `start-time` Sets the subscription start position to the first message
-      with a timestamp greater than or equal to the given instant.
+    - `start-timestamp-nanos` Sets the subscription start position to the first
+      message with a timestamp, in nanoseconds, greater than or equal to the
+      given instant.
 
     - `async-chan` Channel to be used instead of `(clojure.core.async/chan)`.
 
@@ -71,7 +70,9 @@
     future, there will be functionality for consuming all partitions.
 
     If an error occurs during consumption, the throwable object is published to
-    the channel and the channel is closed.")
+    the channel and the channel is closed.
+
+    To close the subscription, shut down the underlying channel.")
 
   (publish [this subject message]
     "Publishes `message` to the NATS `subject`. This is a work in progress."))
@@ -80,7 +81,7 @@
   ^Api$SubscribeRequest$Builder
   [^Api$SubscribeRequest$Builder
    subscribe-request-builder
-   start-at {:keys [start-offset start-time]}]
+   start-at {:keys [start-offset start-timestamp-nanos]}]
   (case start-at
     :liftbridge.start-at/new-only
     (.setStartPosition subscribe-request-builder
@@ -99,61 +100,61 @@
         (.setStartPosition Api$StartPosition/OFFSET)
         (.setStartOffset start-offset))
 
-    :liftbridge.start-at/time
+    :liftbridge.start-at/timestamp
     (-> subscribe-request-builder
         (.setStartPosition Api$StartPosition/TIMESTAMP)
-        (.setStartTimestamp (.getTime ^java.util.Date start-time)))
+        (.setStartTimestamp start-timestamp-nanos))
 
     nil subscribe-request-builder))
 
-(defn- chan->observer ^StreamObserver [chan]
+(defn- chan->observer ^StreamObserver [chan m]
   (reify StreamObserver
     (onNext [_ v]
-      (async/>!! chan v))
+      #p "chego?"
+      (async/>!! chan (m v)))
     (onError [_ ex]
+      #p "erro?"
       (async/>!! chan ex)
       (async/close! chan))
     (onCompleted [_]
-      (async/close! chan))))
+      #p "completo?"
+      #_(async/close! chan))))
 
-(defn- async-call-chan [f & channel]
-  (let [channel (or channel (async/chan))]
-    (f (chan->observer channel))
-    channel))
-
-(defn- async-call-future [f]
-  (future (let [res (async/<!! (async-call-chan f))]
-            (if (instance? Throwable res)
-              (throw res)
-              res))))
+(defn- <?? [c]
+  (let [res (async/<!! c)]
+    (if (instance? Throwable res)
+      (throw res)
+      res)))
 
 (defrecord Client [channel ^APIGrpc$APIStub async-stub]
   IClient
   (create-stream
     [this stream-name {:keys [subject replication-factor group partitions]}]
-    (let [req-obj (cond-> (Api$CreateStreamRequest/newBuilder)
-                    replication-factor (.setReplicationFactor replication-factor)
-                    group              (.setGroup group)
-                    partitions         (.setPartitions partitions)
-                    :default           (-> (.setName stream-name)
-                                           (.setSubject (or subject stream-name))
-                                           .build))]
-      (async-call-future #(.createStream async-stub
-                                         req-obj
-                                         %))))
+    (let [req-obj  (cond-> (Api$CreateStreamRequest/newBuilder)
+                     replication-factor (.setReplicationFactor replication-factor)
+                     group              (.setGroup group)
+                     partitions         (.setPartitions partitions)
+                     :default           (-> (.setName stream-name)
+                                            (.setSubject (or subject stream-name))
+                                            .build))
+          chan     (async/chan)]
+      (->> (chan->observer chan identity)
+           (.createStream async-stub req-obj))
+      (future (<?? chan))))
 
   (subscribe [this stream-name {:keys [partition-number start-at
                                        start-offset start-time
-                                       handler-fn async-chan]
-                                :as opts}]
+                                       async-chan]
+                                :as   opts}]
     (let [request-obj (cond-> (Api$SubscribeRequest/newBuilder)
                         partition-number (.setPartition partition-number)
                         start-at         (set-subscription-starting-point start-at opts)
                         :default         (-> (.setStream stream-name)
-                                             .build))]
-      (async-call-chan #(.subscribe async-stub
-                                    request-obj
-                                    %))))
+                                             .build))
+          chan        (or async-chan (async/chan))]
+      (->> (chan->observer chan liftbridge.message/from-object)
+           (.subscribe async-stub request-obj))
+      chan))
 
   (publish [this subject msg]
     (let [msg            (-> (Api$Message/newBuilder)
@@ -161,8 +162,11 @@
                              (.setValue (ByteString/copyFrom (bytes msg))))
           request-object (-> (Api$PublishRequest/newBuilder)
                              (.setMessage msg)
-                             .build)]
-      (async-call-future #(.publish async-stub request-object %)))))
+                             .build)
+          chan (async/chan)]
+      (->> (chan->observer chan identity)
+           (.publish async-stub request-object))
+      (future (<?? chan)))))
 
 (defn connect
   "Attempts to connect to a Liftbridge server with multiple options"
@@ -174,11 +178,17 @@
   (def chan (->> (ManagedChannelBuilder/forAddress "localhost" 9292)
                    .usePlaintext
                    .build))
+
   (def client (connect chan))
   @(create-stream client "one-stream" {})
 
-
   (def subs-chan (subscribe client "one-stream" {}))
   @(publish client "one-stream" (.getBytes "testing"))
-  (async/<!! subs-chan)
+
+  (def msg (async/<!! subs-chan))
+  (do @(publish client "one-stream" (.getBytes "testing"))
+      (async/<!! subs-chan))
+
+  (.shutdown chan)
+
   )
---
+++ src/main/clojure/liftbridge/message.clj
@@ -0,0 +1,26 @@
+(ns liftbridge.message
+  (:import [liftbridge.proto Api$Message Api$AckPolicy]
+           [java.util Date])
+  (:require [clojure.instant :as instant]))
+
+(defn assoc-when-not-empty [coll k v]
+  (if (empty? v)
+    coll
+    (assoc coll k v)))
+
+(defn ack-policy-from-obj [^Api$AckPolicy obj]
+  (condp = obj
+    Api$AckPolicy/LEADER :liftbridge.ack-policy/leader
+    Api$AckPolicy/ALL    :liftbridge.ack-policy/all
+    Api$AckPolicy/NONE   :liftbridge.ack-policy/none))
+
+(defn from-object [^Api$Message obj]
+  (-> {::offset          (.getOffset obj)
+       ::key             (.toByteArray (.getKey obj))
+       ::value           (.toByteArray (.getValue obj))
+       ::timestamp-nanos (.getTimestamp obj)
+       ::headers         (.getHeaders obj)
+       ::ack-policy      (ack-policy-from-obj (.getAckPolicy obj))}
+      (assoc-when-not-empty ::nat-reply-subject (.getReply obj))
+      (assoc-when-not-empty ::nats-ack-inbox (.getAckInbox obj))
+      (assoc-when-not-empty ::correlation-id (.getCorrelationId obj))))
