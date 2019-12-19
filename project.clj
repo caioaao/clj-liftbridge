326ef01e5fe947cee6bc9808e780c284df75aa36
Caio Oliveira <caioaao@gmail.com>
2019-12-18 19:19:41 -0500

    gen again with better java package

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :manifest {"GIT_COMMIT"   ~(System/getenv "GIT_COMMIT")
              "BUILD_NUMBER" ~(System/getenv "BUILD_NUMBER")}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.10.0"]
